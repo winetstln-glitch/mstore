0a1637bcc4a14494537cabeba8834d3e426ac5b7
winetstln-glitch <winetstln@gmail.com>
2026-02-13 11:32:41 +0700

    Fix: set X-CSRF-TOKEN di axios defaults untuk cegah error 419

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,3 +2,9 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+// Set CSRF Token header to avoid 419 (Page Expired) on POST/PUT/DELETE
+const tokenMeta = document.querySelector('meta[name=\"csrf-token\"]');
+if (tokenMeta && tokenMeta.content) {
+    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = tokenMeta.content;
+}
