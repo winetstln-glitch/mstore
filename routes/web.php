26eb2d3871803809ce729e71ba04ce8f26edd290
winetstln-glitch <winetstln@gmail.com>
2026-03-05 04:08:46 +0700

    Update_route

--- routes/web.php
+++ routes/web.php
@@ -149,6 +149,7 @@ Route::middleware('auth')->group(function () {
     Route::resource('olt', OLTController::class);
     // ... kode router lainnya ...
     Route::post('routers/{router}/pppoe/disconnect', [RouterController::class, 'disconnectPppoe'])->name('routers.pppoe.disconnect');
+    // Route untuk test koneksi router
     Route::post('routers/{router}/test-connection', [RouterController::class, 'testConnection'])->name('routers.test-connection');
     Route::post('routers/{router}/pppoe/toggle-secret', [RouterController::class, 'togglePppoeSecret'])->name('routers.pppoe.toggle-secret');
 
