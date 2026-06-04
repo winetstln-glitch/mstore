3acf8e45fef4dec014474d333882f7fab5499e15
winetstln-glitch <winetstln@gmail.com>
2026-06-04 20:04:17 +0700

    feat: add Indonesian auth, pagination, and password translations

---
+++ lang/id/pagination.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Baris Bahasa Paginasi
+    |--------------------------------------------------------------------------
+    |
+    | Baris bahasa berikut digunakan oleh perpustakaan paginator untuk membangun
+    | tautan paginasi sederhana. Anda bebas mengubahnya sesuai keinginan
+    | Anda untuk menyesuaikan tampilan Anda agar lebih sesuai dengan aplikasi.
+    |
+    */
+
+    'previous' => '&laquo; Sebelumnya',
+    'next' => 'Selanjutnya &raquo;',
+
+];
