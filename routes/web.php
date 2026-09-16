ae29bf86a0147a9b213a1c57362962d547d84443
winets.id <dhedyds9@gmail.com>
2026-09-16 17:03:55 +0700

    Add temporary route to reset ATK data on server

--- routes/web.php
+++ routes/web.php
@@ -1392,3 +1392,23 @@ Route::get('/webhooks/payment/return', [\App\Http\Controllers\PaymentController:
 
 Route::get('/wash/member/verify/{token}', [\App\Http\Controllers\WashMemberController::class, 'verify'])
     ->name('wash.members.verify');
+
+
+Route::get('/reset-atk-danger-xxx', function() {
+    \Illuminate\Support\Facades\DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+    \Illuminate\Support\Facades\DB::table('atk_transactions')->truncate();
+    \Illuminate\Support\Facades\DB::table('atk_transaction_items')->truncate();
+    \Illuminate\Support\Facades\DB::table('atk_products')->truncate();
+    \Illuminate\Support\Facades\DB::table('atk_customers')->truncate();
+    \Illuminate\Support\Facades\DB::table('atk_suppliers')->truncate();
+    \Illuminate\Support\Facades\DB::table('atk_categories')->truncate();
+    \Illuminate\Support\Facades\DB::table('atk_services')->truncate();
+    \Illuminate\Support\Facades\DB::table('atk_stock_movements')->truncate();
+    \Illuminate\Support\Facades\DB::table('atk_expense_categories')->truncate();
+    \Illuminate\Support\Facades\DB::table('atk_expenses')->truncate();
+    \Illuminate\Support\Facades\DB::table('atk_float_accounts')->truncate();
+    \Illuminate\Support\Facades\DB::table('atk_float_transactions')->truncate();
+    \Illuminate\Support\Facades\DB::table('atk_cash_movements')->truncate();
+    \Illuminate\Support\Facades\DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+    return 'Semua Data ATK Berhasil Dihapus! Silakan hapus URL ini dari routes/web.php jika sudah selesai.';
+});
